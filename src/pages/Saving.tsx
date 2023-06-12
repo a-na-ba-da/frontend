aea1b3ad9c695a6a6894604a069e8e10cb66b52d
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-06-12 16:48:29 +0900

    refactor: Footer 컴포넌트 분리
    Saving 컴포넌트에서 Footer 컴포넌트를 분리해 가독성과 재사용성을 높임

--- src/pages/Saving.tsx
+++ src/pages/Saving.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Content from '../components/Content';
+import Footer from '../components/Footer';
 
 const Saving = () => {
   return (
@@ -14,28 +15,7 @@ const Saving = () => {
         <MenuCol>같이 알아요</MenuCol>
         <Content />
       </Main>
-      <Footer>
-        <NavItem>
-          <NavImg></NavImg>
-          <NavText>아껴쓰기</NavText>
-        </NavItem>
-        <NavItem>
-          <NavImg></NavImg>
-          <NavText>나눠쓰기</NavText>
-        </NavItem>
-        <NavItem>
-          <NavImg></NavImg>
-          <NavText>바꿔쓰기</NavText>
-        </NavItem>
-        <NavItem>
-          <NavImg></NavImg>
-          <NavText>다시쓰기</NavText>
-        </NavItem>
-        <NavItem>
-          <NavImg></NavImg>
-          <NavText>마이페이지</NavText>
-        </NavItem>
-      </Footer>
+      <Footer />
     </SavingLayout>
   );
 };
@@ -57,32 +37,4 @@ const MenuCol = styled.div`
   height: 50px;
   justify-content: center;
   align-items: center;
-`;
-
-const Footer = styled.footer`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  height: 78px;
-  border-top: solid 1px #f0f0f0;
-  display: flex;
-  justify-content: space-around;
-`;
-
-const NavItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const NavImg = styled.div`
-  width: 32px;
-  height: 32px;
-  background-color: #999999;
-`;
-
-const NavText = styled.div`
-  font-size: 12px;
-  margin-top: 5px;
 `;
