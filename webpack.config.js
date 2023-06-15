f382d06fc4bdc4393470a9e051076e5741d8c223
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-06-15 18:16:41 +0900

    build: 이미지 import  관련 웹팩 설정
    이미지를 import 하기위해서 웹팩 바벨 추가 설정이 필요하여 라이브러리를 이용해 설정 완료

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,35 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: ['babel-loader', 'ts-loader'],
+      }, 
+      {
+        // 이미지 import 관련 웹팩 설정
+        // 파일이 10k 보다 작을 경우 url-loader 문자열 번들링, 클 경우 file-loader로 파일 복사
+        test: /\.(jpg|jpeg|gif|png|svg|ico)?$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              fallback: 'file-loader',
+              name: 'asset/img/[name].[ext]',
+            },
+          },
+        ],
       },
+      // {
+        // 폰트 import 관련 웹팩 설정
+        // 파일이 10k 보다 작을 경우 url-loader 문자열 번들링, 클 경우 file-loader로 파일 복사
+        // test: /\.(woff|woff2|eot|ttf|otf)$/,
+        // use: [
+        //   {
+        //     loader: 'url-loader',
+        //     options: {
+        //       limit: 10000,
+        //       fallback: 'file-loader',
+        //       name: 'asset/font/[name].[ext]',
+        //     },
+        //   },
     ],
   },
   resolve: {
