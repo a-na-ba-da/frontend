edc1f89dab8af3a47351da1df7e9b551a45b6d27
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-06-16 19:13:31 +0900

    refactor: BuyingPost에서 Comment 컴포넌트 분리
    page 컴포넌트인 BuyingPost에서 Comment 컴포넌트를 분리해 재사용성 증가

--- src/pages/Saving/BuyingPost.tsx
+++ src/pages/Saving/BuyingPost.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
-import SmsOutlinedIcon from '@mui/icons-material/SmsOutlined';
 
 import Button from '../../components/Button';
-import Send from '../../asset/img/send.png';
+import Comment from '../../components/Comment';
 
 const BuyingPost = () => {
   return (
@@ -32,39 +31,8 @@ const BuyingPost = () => {
             올리브영 닥터지 썬크림 같이 사실분 구합니다.
           </MainTextBox>
         </ContentSection>
-        <CommentSection>
-          <SmsOutlinedIcon
-            sx={{ fontSize: 16, color: '#216583', verticalAlign: 'middle' }}
-          />
-          <CommentCountText>2</CommentCountText>
-          <CommentList>
-            <CommentItem>
-              <CommentUserBox>
-                <CommentUserImg>
-                  <AccountCircleTwoToneIcon sx={{ fontSize: 16 }} />
-                </CommentUserImg>
-                <CommentText>파란하늘932</CommentText>
-              </CommentUserBox>
-              <CommentDateText>2020.11.06</CommentDateText>
-              <CommentText>저 살래요</CommentText>
-            </CommentItem>
-            <CommentItem>
-              <CommentUserBox>
-                <CommentUserImg>
-                  <AccountCircleTwoToneIcon sx={{ fontSize: 16 }} />
-                </CommentUserImg>
-                <CommentText>파란하늘932</CommentText>
-              </CommentUserBox>
-              <CommentDateText>2020.11.06</CommentDateText>
-              <CommentText>저 살래요</CommentText>
-            </CommentItem>
-          </CommentList>
-        </CommentSection>
+        <Comment />
       </Main>
-      <CommentInputSection>
-        <CommentSendImg alt="cooment_send" src={Send} />
-        <CommentInput placeholder="댓글을 입력하세요." />
-      </CommentInputSection>
     </PostLayout>
   );
 };
@@ -142,77 +110,4 @@ const PriceText = styled.span`
   font-size: 16px;
   display: inline;
   vertical-align: middle;
-`;
-
-const CommentSection = styled.section``;
-
-const CommentCountText = styled.span`
-  color: #216583;
-  font-size: 13px;
-  margin-left: 5px;
-`;
-
-const CommentList = styled.ul`
-  all: inherit;
-  margin-top: 10px;
-`;
-
-const CommentItem = styled.li`
-  all: inherit;
-  position: relative;
-  padding: 10px;
-  margin: 0;
-  &:first-child {
-    border-top: 1px solid #e1e1e1;
-  }
-  &:not(:first-child) {
-    border-top: 1px solid #e1e1e1;
-    border-bottom: 1px solid #e1e1e1;
-  }
-`;
-
-const CommentUserBox = styled.div`
-  display: flex;
-`;
-
-const CommentUserImg = styled.div`
-  margin-right: 5px;
-`;
-
-const CommentText = styled.span`
-  font-size: 13px;
-`;
-
-const CommentDateText = styled.span`
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  color: #999999;
-  font-size: 13px;
-`;
-
-const CommentInputSection = styled.section`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
-  height: 56px;
-  background-color: #fbfbfb;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-
-const CommentSendImg = styled.img`
-  position: absolute;
-  right: 25px;
-`;
-
-const CommentInput = styled.input`
-  box-sizing: border-box;
-  padding-left: 10px;
-  border: 1px solid #e7e7e7;
-  border-radius: 7px;
-  width: 100%;
-  height: 34px;
-  margin: 0 15px;
 `;
