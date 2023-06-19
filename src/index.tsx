dbf19ec00cec29da2bc5d02c3ca76f17cb7e20f9
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-06-19 18:31:48 +0900

    styled: react-router-dom 관련 설정 누락 추가
    라우터 설정 관련하여 누락된 코드 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { theme } from './theme';
 import { ThemeProvider } from '@mui/material';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ThemeProvider>,
   document.getElementById('root'),
 );
