857d188fe7a06d41e6ea5f701f5af4c61325dc94
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-06-20 10:53:41 +0900

    feact: 같이 알아요 작성 페이지 UI
    아껴쓰기 같이 알아요 작성 페이지의 초기 UI 구축

---
+++ src/pages/Saving/KnowingPostEdit.tsx
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import {
+  FormControl,
+  FormControlLabel,
+  Radio,
+  RadioGroup,
+} from '@mui/material';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
+import CloseIcon from '@mui/icons-material/Close';
+
+import Header from '../../components/Header/Header';
+import HeaderLeft from '../../components/Header/HeaderLeft';
+import HeaderRight from '../../components/Header/HeaderRight';
+
+export default function KnowingPostEdit() {
+  const [selectedMethod, SetSelectedMethod] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    SetSelectedMethod(e.target.value);
+  };
+
+  return (
+    <KnowingPostEditLayout>
+      <Header title="같이 알아요">
+        <HeaderLeft>
+          <CloseIcon color="primary" />
+        </HeaderLeft>
+        <HeaderRight>
+          <ConfirmText>확인</ConfirmText>
+        </HeaderRight>
+      </Header>
+      <Line />
+      <Main>
+        <ImgBox>
+          <CameraAltIcon />
+          <ImgCountBox>0 / 10</ImgCountBox>
+        </ImgBox>
+        <InputBox>
+          <Input placeholder="글 제목"></Input>
+          <CheckBox>
+            <div>구매처</div>
+            <FormControl>
+              <RadioGroup row value={selectedMethod} onChange={handleChange}>
+                <FormControlLabel
+                  value="offline"
+                  control={<Radio />}
+                  label="오프라인"
+                />
+                <FormControlLabel
+                  value="online"
+                  control={<Radio />}
+                  label="온라인"
+                />
+              </RadioGroup>
+            </FormControl>
+          </CheckBox>
+          {selectedMethod === 'online' ? (
+            <Input placeholder="행사 URL"></Input>
+          ) : selectedMethod === 'offline' ? (
+            <Input placeholder="행사 위치"></Input>
+          ) : null}
+          <TextArea placeholder="다른 사람과 같이 구매하고 싶은 상품에 대해&#13;설명해주세요 :)"></TextArea>
+        </InputBox>
+      </Main>
+    </KnowingPostEditLayout>
+  );
+}
+
+const KnowingPostEditLayout = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+const ConfirmText = styled.span`
+  font-size: 18px;
+  color: #8f00ff;
+`;
+
+const Line = styled.hr`
+  background: #eaeaeb;
+  height: 1px;
+  border: 0;
+  margin: 0;
+`;
+
+const Main = styled.main`
+  padding: 0 15px;
+`;
+
+const ImgBox = styled.div`
+  width: 60px;
+  height: 60px;
+  border: 1px solid #dedde2;
+  border-radius: 5px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 15px 0;
+`;
+
+const ImgCountBox = styled.div`
+  font-size: 12px;
+  margin-top: 2px;
+`;
+
+const InputBox = styled.div`
+  & > :first-child {
+    border-top: 1px solid #f3f3f3;
+    border-bottom: 1px solid #f3f3f3;
+  }
+  & > :not(:first-child) {
+    border-bottom: 1px solid #f3f3f3;
+  }
+  & > :last-child {
+    border-bottom: 0;
+  }
+`;
+
+const Input = styled.input`
+  width: 100%;
+  border: 0;
+  height: 50px;
+  box-sizing: border-box;
+  outline: none;
+  font-size: 15px;
+  &::placeholder {
+    color: #d1d3d7;
+  }
+`;
+
+const CheckBox = styled.div`
+  font-size: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  border: 0;
+`;
+
+const TextArea = styled.textarea`
+  resize: none;
+  outline: none;
+  box-sizing: border-box;
+  width: 100%;
+  height: 200px;
+  border: 0;
+  margin-top: 15px;
+  ::placeholder {
+    font-size: 15px;
+    color: #d1d3d7;
+  }
+`;
