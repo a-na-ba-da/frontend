5ee0e70585bdc348ef46fa770f3bcd67dba355d0
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-06-21 13:44:46 +0900

    refactor: basURL 분리
    다른 컴포넌트에서 baseURL을 직접 접근할 수 있도록 basURL을 따로 분리

---
+++ src/api/basURL.ts
@@ -0,0 +1,3 @@
+const baseURL = 'http://172.105.237.249:8080/api/v1';
+
+export default baseURL;
--- src/api/defaultClient.ts
+++ src/api/defaultClient.ts
@@ -1,7 +1,6 @@
 // axios 관련 설정
 import axios, { type Axios } from 'axios';
-
-const baseURL = 'http://172.105.237.249:8080/api/v1';
+import baseURL from './basURL';
 
 const defaultClient: Axios = axios.create({
   baseURL,
