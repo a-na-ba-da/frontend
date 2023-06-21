ba4301ad53a3c93567cefcc3aa2c1160d5875658
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-06-21 16:21:52 +0900

    style: 커스텀 컴포넌트 개행
    외부 라이브러리 import 와 내부 커스텀 컴포넌트 분리 개행 적용

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import React, { Children, ReactNode, isValidElement } from 'react';
 import styled from 'styled-components';
+
 import HeaderLeft from './HeaderLeft';
 import HeaderRight from './HeaderRight';
 
