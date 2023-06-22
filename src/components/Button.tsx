1205445bac66cba23e690af1b478976ca7335ec7
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-06-23 00:14:04 +0900

    fix: React does not recognize the props 오류 해결
    오류를 해결하기 위해 styled-components로 스타일을 커스텀하는 대신 sx 속성에 스타일을 직접 전달하는 방식으로 변경

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
-import { StyledEngineProvider } from '@mui/material';
 import MuiButton from '@mui/material/Button';
 
 interface ButtonProps {
@@ -19,40 +17,23 @@ export default function Button({
   borderColor,
 }: ButtonProps) {
   return (
-    <StyledEngineProvider injectFirst>
-      <Btn
-        textColor={textColor}
-        textSize={textSize}
-        backgroundColor={backgroundColor}
-        borderColor={borderColor}
-      >
-        {content}
-      </Btn>
-    </StyledEngineProvider>
+    <MuiButton
+      sx={{
+        height: 30,
+        borderRadius: '5px',
+        color: textColor ? textColor : 'white',
+        fontSize: textSize ? textSize : '13px',
+        backgroundColor: backgroundColor ? backgroundColor : '#8f00ff',
+        border: borderColor ? `1px solid ${borderColor}` : 'none',
+        '&:hover': {
+          backgroundColor: backgroundColor ? backgroundColor : '#8f00ff',
+        },
+        '&:active': {
+          backgroundColor: backgroundColor ? backgroundColor : '#8f00ff',
+        },
+      }}
+    >
+      {content}
+    </MuiButton>
   );
 }
-
-interface BtnProps {
-  textColor?: string;
-  textSize?: string;
-  backgroundColor?: string;
-  borderColor?: string;
-}
-
-const Btn = styled(MuiButton)<BtnProps>`
-  background-color: ${(props) =>
-    props.backgroundColor ? props.backgroundColor : '#8f00ff'};
-  color: ${(props) => (props.textColor ? props.textColor : 'white')};
-  font-size: ${(props) => (props.textSize ? props.textSize : '13px')};
-  height: 30px;
-  border: 1px solid ${(props) => (props.borderColor ? props.borderColor : '0')};
-  border-radius: 5px;
-  &:hover {
-    background-color: ${(props) =>
-      props.backgroundColor ? props.backgroundColor : '#8f00ff'};
-  }
-  &:active {
-    background-color: ${(props) =>
-      props.backgroundColor ? props.backgroundColor : '#8f00ff'};
-  }
-`;
