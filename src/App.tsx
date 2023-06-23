a69781c260404fd842e88a3cfe83acd1ffd39ca7
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-06-24 01:35:32 +0900

    refactor: index.tsx 정리
    index.tsx에 있던 기능들을 app.tsx으로 넘겨서 index.tsx 구조가 직관적으로 보이게함

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { theme } from './theme';
+import { ThemeProvider } from '@mui/material';
 
 import BuyingPost from './pages/Saving/BuyingPost';
 import BuyingPostEdit from './pages/Saving/BuyingPostEdit';
@@ -9,14 +11,18 @@ import SelectPlace from './pages/Saving/SelectPlace';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SavingBoard />} />
-      <Route path="/saving" element={<SavingBoard />} />
-      <Route path="/saving/buying/:id" element={<BuyingPost />} />
-      <Route path="/saving/buying/write" element={<BuyingPostEdit />} />
-      <Route path="/saving/knowing/:id" element={<KnowingPost />} />
-      <Route path="/saving/Knowing/write" element={<KnowingPostEdit />} />
-      <Route path="/saving/place" element={<SelectPlace />} />
-    </Routes>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SavingBoard />} />
+          <Route path="/saving" element={<SavingBoard />} />
+          <Route path="/saving/buying/:id" element={<BuyingPost />} />
+          <Route path="/saving/buying/write" element={<BuyingPostEdit />} />
+          <Route path="/saving/knowing/:id" element={<KnowingPost />} />
+          <Route path="/saving/Knowing/write" element={<KnowingPostEdit />} />
+          <Route path="/saving/place" element={<SelectPlace />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,5 @@
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import { theme } from './theme';
-import { ThemeProvider } from '@mui/material';
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
