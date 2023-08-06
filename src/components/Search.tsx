963660cd464f265873347eeb4ed178f84cb88705
khsoo2439@gmail.com <khsoo2439@gmail.com>
2023-08-06 23:02:55 +0900

    feat: 검색 기능
    현재 페이지 위치에 따른 검색 기능 추가

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { KeyboardEvent, useState } from 'react';
 import styled from 'styled-components';
 import SearchIcon from '@mui/icons-material/Search';
 
+import { useAppDispatch, useAppSelector } from '../hooks/redux';
+import { fetchBuyingPostList } from '../context/reducer/buyingReducer';
+import { fetchKnowingPostList } from '../context/reducer/knowingReducer';
+
 export default function Search() {
+  const dispatch = useAppDispatch();
+  const [keyword, setKeyword] = useState<string>('');
+  const menuType = useAppSelector((state) => state.menu.menuType);
+  const [isKeyDown, setIsKeyDown] = useState(false);
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter' && !e.nativeEvent.isComposing && !isKeyDown) {
+      setIsKeyDown(true);
+      // 키워드가 없을 경우 전체 검색
+      if (keyword.length === 0) {
+        switch (menuType) {
+          case 'buying':
+            dispatch(fetchBuyingPostList());
+            break;
+          case 'knowing':
+            dispatch(fetchKnowingPostList());
+            break;
+        }
+      } else if (keyword.length > 1) {
+        switch (menuType) {
+          case 'buying':
+            dispatch(fetchBuyingPostList(keyword));
+            break;
+          case 'knowing':
+            dispatch(fetchKnowingPostList(keyword));
+            break;
+        }
+      } else alert('2자 이상부터 검색 가능합니다.');
+    }
+  };
+
+  const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setIsKeyDown(false);
+    }
+  };
+
   return (
     <SearchLayout>
-      <SearchInputBox placeholder="검색어를 입력해주세요" />
+      <SearchInputBox
+        placeholder="검색어를 입력해주세요"
+        onChange={(e) => setKeyword(e.target.value)}
+        value={keyword}
+        onKeyDown={handleKeyDown}
+        onKeyUp={handleKeyUp}
+      />
       <SearchImg>
         <SearchIcon style={{ verticalAlign: 'middle' }} />
       </SearchImg>
